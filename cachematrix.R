7f42a6ebe927334204cb543c860b439046ff6e69
Archelangelo <medycc@gmail.com>
2015-10-17 21:36:21 -0400

    Two functions are written according to the problem requirement

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Creating a list with functions defined in an environment
+## where data are saved.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ivr <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    ivr <<- NULL
+  }
+  
+  get <- function() x
+  
+  inv <- function() ivr
+  
+  setinv <- function(v) ivr <<- v
+  
+  list(set = set, get = get, setinv = setinv, inv = inv)
 }
 
 
-## Write a short comment describing this function
+## Get the cached inverse or calculate it and chache the result.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  ivr <- x$inv()
+  if (!is.null(ivr)) {
+    message("getting cached data.")
+    return(ivr)
+  }
+  ivr <- solve(x$get(), ...)
+  x$setinv(ivr)
+  ivr
 }
